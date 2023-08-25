9c5e7a035f8b4f49d3efccd000c7ae0ee49054a9
Derstood <1831003848@qq.com>
2023-08-25 19:31:41 +0800

    Create pt.tcl

---
+++ PT_shells/pt.tcl
@@ -0,0 +1,8 @@
+alias rt report_timing -nos -cros -trans -cap -derate -net
+alias rtt rt -to
+alias rtf rt -from
+alias rh rt -delay_type min
+alias rht rh -to
+alias rhf rh -from
+
+alias rg report_global_timing
