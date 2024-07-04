e5d01d696fcebde96b2ad04e5e82d3e283657140
Derstood <1831003848@qq.com>
2024-07-04 15:40:25 +0800

    Update pipe_reg_bounds.tcl

--- PR_shell/pipe_reg_bounds.tcl
+++ PR_shell/pipe_reg_bounds.tcl
@@ -1,4 +1,8 @@
 ##
+remove_bounds critical_io_*
+
+set i 0
 foreach_in_collection p $output_ports {
   create_bound -name critical_io_$i -dimensions 100 -diamond $p [all_fanin -to $p -flat -startpoint_only -only_cells]
+  incr i
 }
