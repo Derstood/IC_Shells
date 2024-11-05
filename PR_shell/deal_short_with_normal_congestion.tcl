b2bacbb945158f1cb6c282b874fdc527b6ad6220
Derstood <1831003848@qq.com>
2024-11-05 11:47:52 +0800

    Create deal_short_with_normal_congestion.tcl

---
+++ PR_shell/deal_short_with_normal_congestion.tcl
@@ -0,0 +1,3 @@
+set_app_options -list {place_opt.flow.optimize_layers false}
+set_app_options -list {place_opt.flow.optimize_layers_critical_range 0.0001}
+set_app_options -list {place_opt.flow.optimize_ndr false}
