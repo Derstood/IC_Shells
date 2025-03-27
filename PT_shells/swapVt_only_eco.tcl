d1ff4bcd4d2c74feeb9dbcb4c88d9aac092cea6f
derstood_zxw <gmfufuafufu@gmail.com>
2025-03-27 17:03:50 +0800

    update swapVt only

--- PT_shells/swapVt_only_eco.tcl
+++ PT_shells/swapVt_only_eco.tcl
@@ -2,7 +2,13 @@ set eco_alternative_area_ratio_threshold 1
 
 define_user_attribute -class lib_cell -type string eco_only_swapVt
 set_app_var eco_alternative_cell_attribute_restriction eco_only_swapVt
+
+
 remote_execute {
+    set eco_alternative_area_ratio_threshold 1
+
+    define_user_attribute -class lib_cell -type string eco_only_swapVt
+    set_app_var eco_alternative_cell_attribute_restriction eco_only_swapVt
     foreach_in_coll cell [get_lib_cells */*] {
       set base_name [get_attr $cell base_name]
       set full_name [get_attr $cell full_name]
