2a0457858a000096b2a05325ebfc8e238aeab810
Derstood <1831003848@qq.com>
2025-04-24 10:19:40 +0800

    Update timing_derate.tcl

--- PT_shells/timing_derate.tcl
+++ PT_shells/timing_derate.tcl
@@ -7,3 +7,14 @@ set_timing_derate –data –cell_delay –early $derateValue  [get_lib_cells  *
 set_timing_derate –data –cell_delay –late  $derateValue  [get_lib_cells  *rvt*/* ]
 set_timing_derate –data –net_delay  –early $derateValue  [get_lib_cells  *rvt*/* ]
 set_timing_derate –data –net_delay  –late  $derateValue  [get_lib_cells  *rvt*/* ]
+
+
+set_timing_derate -incr –clock –cell_delay –early $derateValue  [get_lib_cells  *rvt*/* ]
+set_timing_derate -incr –clock –cell_delay –late  $derateValue  [get_lib_cells  *rvt*/* ]
+set_timing_derate -incr –clock –net_delay  –early $derateValue  [get_lib_cells  *rvt*/* ]
+set_timing_derate -incr –clock –net_delay  –late  $derateValue  [get_lib_cells  *rvt*/* ]
+
+set_timing_derate -incr –data –cell_delay –early $derateValue  [get_lib_cells  *rvt*/* ]
+set_timing_derate -incr –data –cell_delay –late  $derateValue  [get_lib_cells  *rvt*/* ]
+set_timing_derate -incr –data –net_delay  –early $derateValue  [get_lib_cells  *rvt*/* ]
+set_timing_derate -incr –data –net_delay  –late  $derateValue  [get_lib_cells  *rvt*/* ]
